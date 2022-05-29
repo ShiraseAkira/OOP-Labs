8b13cd977763300c56108bd7392637d7af8736d7
Maxim Laptev <justsaynya@gmail.com>
2022-05-29 20:51:47 +0300

    argument parsing moved to separate function

--- Lab_1/task_1_V3_findtext/task_1_V3_findtext.cpp
+++ Lab_1/task_1_V3_findtext/task_1_V3_findtext.cpp
@@ -1,35 +1,53 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <optional>
 
 using namespace std;
 
-int main(int argc, char* argv[])
+struct Args
 {
+	string inputFileName;
+	string stringToFind;
+};
+
+optional<Args> ParseArgs(int argc, char* argv[]) {
 	if (argc != 3)
 	{
-		cout << "Invalid arguments count" << endl;
+		cout << "Invalid arguments count\n";
 		cout << "Usage: findtext.exe <file name> <line to search>" << endl;
+		return nullopt;
+	}
+	Args args;
+	args.inputFileName = argv[1];
+	args.stringToFind = argv[2];
+	return args;
+}
+
+int main(int argc, char* argv[])
+{
+	auto args = ParseArgs(argc, argv);
+
+	if (!args)
+	{
 		return 1;
 	}
 
 	std::ifstream file;
-	file.open(argv[1]);
-
+	file.open(args->inputFileName);
 	if (!file.is_open())
 	{
 		cout << "Can not open " << argv[1] << endl;
 		return 1;
 	}
 
-	string stringToFind = argv[2];
 	string s;
 	bool isFound = false;
 	int count = 1;
 
 	while (getline(file, s))
 	{
-		if (s == stringToFind)
+		if (s == args->stringToFind)
 		{
 			isFound = true;
 			cout << count << endl;

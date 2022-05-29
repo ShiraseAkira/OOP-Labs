d1da5f19be1d192574331ab8615bb060242f5902
Maxim Laptev <justsaynya@gmail.com>
2022-05-29 19:11:39 +0300

    add ckeck for invalid argument count and test

--- Lab_1/task_1_V3_findtext/tests/test.bat
+++ Lab_1/task_1_V3_findtext/tests/test.bat
@@ -3,7 +3,15 @@
 set PROGRAM="%~1"
 set OUT="%TEMP%\out.txt"
 
-%PROGRAM% > %OUT% || goto error
+REM testing program to tell about invalid usage
+%PROGRAM% > %OUT% && goto error
+fc %OUT% %~dp0InvalidArguments.txt > nil || goto error
+
+%PROGRAM% arg1 arg2 arg3 > %OUT% && goto error
+fc %OUT% %~dp0InvalidArguments.txt > nil || goto error
+
+
+
 
 echo All tests passed
 exit /B 0

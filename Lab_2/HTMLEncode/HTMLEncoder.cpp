6e5295606ff2dddc28aa28b31b8faf241d0bf018
Maxim Laptev <justsaynya@gmail.com>
2022-06-05 20:35:55 +0300

    Encoder realisation

--- Lab_2/HTMLEncode/HTMLEncoder.cpp
+++ Lab_2/HTMLEncode/HTMLEncoder.cpp
@@ -2,11 +2,40 @@
 
 std::string HTMLEncode(const std::string& text)
 {
-	std::string s = text;
-	int pos;
-	while (pos = s.find_first_of("\"`<>&")) {
-		switch (s[pos]) {
-		case "\":
+	std::string s;
+	int  loc, pos = 0;
+
+	while (true) {
+		loc = text.find_first_of("\"'<>&", pos);
+		if (loc == std::string::npos) {
+			s.append(text, pos, text.length() - pos);
+			break;
+		}
+		else {
+			s.append(text, pos, loc - pos);
+
+			switch (text[loc]) {
+			case '"': 
+				s.append("&quot;");
+				break;
+			case '\'':
+				s.append("&apos;");
+				break;
+			case '<':
+				s.append("&lt;");
+				break;
+			case '>':
+				s.append("&gt;");
+				break;
+			case '&':
+				s.append("&amp;");
+				break;
+			}
+
+			pos = loc + 1;
+			loc = text.find_first_of("\"`<>&", pos);
 		}
 	}
+
+	return s;
 }

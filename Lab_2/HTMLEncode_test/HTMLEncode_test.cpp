5ed849f735a2871986fd72c2c6817e5e02291494
Maxim Laptev <justsaynya@gmail.com>
2022-06-05 20:34:24 +0300

    tests for encoding

--- Lab_2/HTMLEncode_test/HTMLEncode_test.cpp
+++ Lab_2/HTMLEncode_test/HTMLEncode_test.cpp
@@ -4,7 +4,7 @@
 #include "../catch.hpp"
 #include "../HTMLEncode/HTMLEncoder.h"
 
-TEST_CASE("Generating prime numbers")
+TEST_CASE("Encoding strings for HTML")
 {
 	
 	SECTION("Empty string encoded as empty string"){
@@ -15,4 +15,31 @@ TEST_CASE("Generating prime numbers")
 		std::string s("test string no encode needed");
 		REQUIRE(HTMLEncode(s) == s);
 	}
+	SECTION("'\"' character in string gets replaced by &quot;") {
+		std::string s("test string with \"");
+		REQUIRE(HTMLEncode(s) == "test string with &quot;");
+	}
+	SECTION("''' character in string gets replaced by &apos;") {
+		std::string s("test string with '");
+		REQUIRE(HTMLEncode(s) == "test string with &apos;");
+	}
+	SECTION("'<' character in string gets replaced by &lt;") {
+		std::string s("test string with <");
+		REQUIRE(HTMLEncode(s) == "test string with &lt;");
+	}
+	SECTION("'>' character in string gets replaced by &gt;") {
+		std::string s("test string with >");
+		REQUIRE(HTMLEncode(s) == "test string with &gt;");
+	}
+	SECTION("'&' character in string gets replaced by &amp;") {
+		std::string s("test string with &");
+		REQUIRE(HTMLEncode(s) == "test string with &amp;");
+	}
+
+	SECTION("String with multiple characters to encode processed correctly as well") {
+		std::string s("Cat <says> \"Meow\".M & M\'s");
+		REQUIRE(HTMLEncode(s) == "Cat &lt;says&gt; &quot;Meow&quot;.M &amp; M&apos;s");
+	}
+
+	
 }

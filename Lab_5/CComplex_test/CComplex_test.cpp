906b1b9e5e04a1a9ac772b179e378d7f0af34357
Maxim Laptev <justsaynya@gmail.com>
2022-06-11 13:06:20 +0300

    tests for overloaded binary +

--- Lab_5/CComplex_test/CComplex_test.cpp
+++ Lab_5/CComplex_test/CComplex_test.cpp
@@ -22,6 +22,44 @@ SCENARIO("Testing basic functionality") {
 		CHECK(ci.Im() == 1);
 		CHECK(ci.Re() == 0);
 		CHECK(ci.GetMagnitude() == 1);
-		CHECK_THAT(ci.GetArgument(), Catch::Matchers::WithinAbs(3.1415/2, 0.0001));
+		CHECK_THAT(ci.GetArgument(), Catch::Matchers::WithinAbs(3.1415 / 2, 0.0001));
+	}
+	CComplex c(3, 4);
+	GIVEN("complex number with both real and image parth") {
+		CHECK(c.Im() == 4);
+		CHECK(c.Re() == 3);
+		CHECK(c.GetMagnitude() == 5);
+		CHECK_THAT(c.GetArgument(), Catch::Matchers::WithinAbs(0.9273, 0.0001));
+	}
+}
+
+SCENARIO("Testing overloaded binary +") {	
+	GIVEN("2 complex numbers") {
+		CComplex c1(0, 1), c2(1, 0);
+		WHEN("you add them") {
+			CComplex result = c1 + c2;
+			CHECK(result.Im() == 1);
+			CHECK(result.Re() == 1);			
+			CHECK_THAT(result.GetMagnitude(), Catch::Matchers::WithinAbs(1.4142, 0.0001));
+			CHECK_THAT(result.GetArgument(), Catch::Matchers::WithinAbs(3.1415 / 4, 0.0001));
+		}
+	}
+	GIVEN("complex and float number") {
+		CComplex c(3, 3);
+		double f = 1;
+		WHEN("you add float to complex") {
+			CComplex result = c + f;
+			CHECK(result.Im() == 3);
+			CHECK(result.Re() == 4);
+			CHECK(c.GetMagnitude() == 5);
+			CHECK_THAT(c.GetArgument(), Catch::Matchers::WithinAbs(0.6435, 0.0001));
+		}
+		AND_WHEN("you add complex to float") {
+			CComplex result = f + c;
+			CHECK(result.Im() == 3);
+			CHECK(result.Re() == 4);
+			CHECK(c.GetMagnitude() == 5);
+			CHECK_THAT(c.GetArgument(), Catch::Matchers::WithinAbs(0.6435, 0.0001));
+		}
 	}
 }
